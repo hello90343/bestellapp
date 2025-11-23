cf4dc45d51a047329f3129269c732b047a08bf03
Rodri Dem <gr.za@web.de>
2025-11-23 19:54:46 +0100

    StickyMain base finishid

--- bestellapp_files/script.js
+++ bestellapp_files/script.js
@@ -1,3 +1,55 @@
-function addShoppingCart() {
+const allDishesSection = document.getElementById("allDishesSection");
+const stickyMain = document.getElementById("stickyMain");
+const stickyMainDiv = document.getElementById("stickyMainDiv");
 
+function init(){
+    renderPizza()
 }
+
+
+function renderPizza() {
+    allDishesSection.innerHTML = "";
+    allDishesSection.innerHTML += `
+                                   <img id="aDSImg" src="./assets/imgs/allDishesImg.png" alt="Hauptgerichte">
+                                   `;
+
+    for(let i = 0; i < dishes.length; i++) {
+        let keyDishes = dishes[i];
+        let priceChange = keyDishes.price.toFixed(2).replace(".", ",");
+        let html = aDSHtml(keyDishes, priceChange, i);
+                     allDishesSection.innerHTML += html;
+    }
+}
+
+function addShoppingCart() {
+    stickyMainDiv.innerHTML = "";
+
+    let html = `<ul>`;
+
+    for (let j = 0; j < dishes.length; j++) {
+        let keyDishes = dishes[j];
+        if (keyDishes.amount > 0) {
+            html += aDSHtmlOne(keyDishes, j)
+        }
+    }
+
+    html += `</ul>`; 
+    stickyMainDiv.innerHTML = html;
+}
+
+function addMore(i) {
+    dishes[i].amount++;
+    addShoppingCart(i);  
+}
+
+function removeMore(i){
+    dishes[i].amount--;
+    addShoppingCart(i);
+}
+
+function deleteItem(i) {
+    dishes[i].amount = 0;
+    addShoppingCart(i);
+}
+
+
--- bestellapp_files/scripts/templates.js
+++ bestellapp_files/scripts/templates.js
@@ -1,4 +1,4 @@
-function aDSHtml(keyDishes, priceChange){
+function aDSHtml(keyDishes, priceChange, i){
     return ` 
                      <section class="aDSMeal">
                      <article class="aDSMealLeft">
@@ -6,7 +6,28 @@ function aDSHtml(keyDishes, priceChange){
                      <p><span style="font-weight: 100;">${keyDishes.description}</span></p>
                      <p><span style="color: rgb(235, 152, 0); font-weight:600;">${priceChange}${keyDishes.currency}</span></p>
                      </article>
-                     <button onclick="addShoppingCart()" class="aDSMealRight">&#65291;</button>
+                     <button onclick="addMore(${i})" class="aDSMealRight aDSSelection${i}">&#65291;</button>
                      </section>
                      `;
+}å
+
+function aDSHtmlOne (keyDishes, j){
+    return `
+                <li class="aDSMoney">
+                    <h3>${keyDishes.name}</h3>
+                    <div class="aDSBottom">
+                    <div class="aDSAmount">
+                    <button onclick="removeMore(${j})">&minus;</button>
+                    ${keyDishes.amount}x 
+                    <button onclick="addMore(${j})">&plus;</button>
+                    </div>
+                    <div class="aDSMoneyTwo">
+                    ${keyDishes.price.toFixed(2).replace(".", ",")} ${keyDishes.currency}
+                    </div>
+                    <div onclick="deleteItem(${j})" class="aDSTrash">
+                    &#128465; 
+                    </div>
+                    </div>
+                </li>
+            `;
 }
